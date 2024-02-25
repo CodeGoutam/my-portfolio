cda1082ee5c0182fec0c4ccc1e08587f53fd9e9d
CodeGoutam <hgoutam2001@gmail.com>
2024-02-25 17:10:27 +0530

    colors

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -20,24 +20,23 @@ export default function Contact() {
     return (
 
         <>
-            <h1>Contact Form</h1>
+            <h1 style={{ color: 'grey' }}>Contact Form</h1>
             <div className="wpcf7" id="wpcf7-f156-p143-o1 formwrap">
                 <form ref={form} onSubmit={sendEmail} method="post" className="wpcf7-form" novalidate="novalidate">
                     <p>
-                        <span className="wpcf7-form-control-wrap Name">
-                            <input type="text" name="from_name" size="40" className="nameinput wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false" placeholder="Name" />
+                        <span className="contacts wpcf7-form-control-wrap Name">
+                            <input type="text" name="from_name" size="40" className="nameinput wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false" placeholder="Name"
+                          />
                         </span>
                         <span className="wpcf7-form-control-wrap Email">
                             <input type="email" name="from_email" size="40" className="emailinput wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email" aria-required="true" aria-invalid="false" placeholder="Email" />
                         </span>
 
                         <span className="wpcf7-form-control-wrap Message">
-                            <textarea name="message" cols="40" rows="10" className="wpcf7-form-control wpcf7-textarea" aria-invalid="false" placeholder="Message"></textarea>
+                            <textarea name="message" cols="40" rows="10" aria-invalid="false" placeholder="Message"></textarea>
                         </span>
-                        <input type="submit" value="Send" className="wpcf7-form-control wpcf7-submit bttn" />
+                        <button type="submit" value="Send" className="wpcf7-form-control wpcf7-submit bttn">Submit</button>
                     </p>
-                    <div className="wpcf7-response-output wpcf7-display-none">
-                    </div>
                 </form>
             </div>
 
