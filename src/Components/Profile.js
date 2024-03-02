44902bad066fb89e8b156e8599069effbb372218
CodeGoutam <hgoutam2001@gmail.com>
2024-03-02 14:00:51 +0530

    profileupdate2

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { Link } from "react-router-dom";
 import '../CSS/CSS.css'
 import img from '../images/h.png'
+import bg from '../images/b.png'
 
 const Profile = () => {
-    return (
+    return (<div className='profileDiv'>
         <div className='profile rounded-5  d-flex align-items-center  justify-content-around '>
-            <img className='img-fluid rounded-circle' src={img} />
+            <img className='img-fluid rounded-circle ' src={img} />
             <div className=''>
-                <h2 className='fw-bold' style={{ fontSize: "4vw",color:'white' }}>Hemant Goutam</h2>
+                <h2 className='fw-bold' style={{ fontSize: "4vw", color: 'white' }}>Hemant Goutam</h2>
                 <div className='linksDiv d-flex justify-content-between '>
                     <Link className='links' to={'https://github.com/CodeGoutam'} target="_blank">
                         <i class="fa-brands fa-github me-2 "></i> <span className='labels'>Github</span>
@@ -18,7 +19,9 @@ const Profile = () => {
                     </Link>
                 </div>
             </div>
-        </div >)
+        </div >
+    </div>
+    )
 }
 export default Profile
 
