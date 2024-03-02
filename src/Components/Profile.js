c2789c24e88e7f354bf527d3eef055fe9cf698c3
CodeGoutam <hgoutam2001@gmail.com>
2024-03-02 13:32:27 +0530

    profileupdates

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,29 +1,24 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 import '../CSS/CSS.css'
-import img from '../images/g.png'
+import img from '../images/h.png'
+
 const Profile = () => {
-    return (<>
-        <div className='cover w-100 h-auto p-3 rounded-1'>
-            <div className='profile d-flex align-items-center '>
-                <img className='image img-fluid' src={img} alt='Hemant Goutam' />
-                <p className='name'>
-                    <h2 className='text-white fw-bold'>Hemant Goutam</h2>
-                    <div className='linksDiv d-flex justify-content-between '>
-                        <Link className='links' to={'https://github.com/CodeGoutam'} target="_blank">
-                            <i class="fa-brands fa-github me-2 "></i> <span className='labels'>Github</span>
-                        </Link>
-                        <Link className='links' to={'https://www.linkedin.com/in/hemant-goutam/'} target="_blank">
-                            <i class="fa-brands fa-linkedin me-2 "></i> <span className='labels'>LinkedIn</span>
-                        </Link>
-                    </div>
-                </p>
+    return (
+        <div className='profile rounded-5  d-flex align-items-center  justify-content-around '>
+            <img className='img-fluid rounded-circle' src={img} />
+            <div className=''>
+                <h2 className='fw-bold' style={{ fontSize: "4vw",color:'white' }}>Hemant Goutam</h2>
+                <div className='linksDiv d-flex justify-content-between '>
+                    <Link className='links' to={'https://github.com/CodeGoutam'} target="_blank">
+                        <i class="fa-brands fa-github me-2 "></i> <span className='labels'>Github</span>
+                    </Link>
+                    <Link className='links' to={'https://www.linkedin.com/in/hemant-goutam/'} target="_blank">
+                        <i class="fa-brands fa-linkedin me-2 "></i> <span className='labels'>LinkedIn</span>
+                    </Link>
+                </div>
             </div>
-        </div>
-    </>)
+        </div >)
 }
 export default Profile
 
-
-// https://github.com/CodeGoutam
-// https://www.linkedin.com/in/hemant-goutam/
