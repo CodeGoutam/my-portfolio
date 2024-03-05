3a7e30134dd80ed2f0dae02f729f0ebbb295f37e
CodeGoutam <hgoutam2001@gmail.com>
2024-03-05 22:41:35 +0530

    cards

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import '../CSS/CSS.css'
 import { Link } from 'react-router-dom'
+import bg from "../images/bg.jpg"
+
 
 export default function Cards(props) {
     return (<>
-        <Link to={props.link} target="_blank" className='h-auto w-100 rounded shadow text-decoration-none p-1 m-3'>
+        <Link to={props.link} target="_blank" className='h-auto w-100 rounded shadow text-decoration-none p-1 m-3' style={{ backgroundImage: `url(${bg})`, backgroundSize: 'cover', }}>
             <div className='w-75 h-auto m-auto  p-2'>
-                <img className='h-75 w-75 rounded-circle p-2' src={props.img} />
+                <img className='h-75 w-75 rounded-circle p-2' style={{ border: "solid 2px white" }} src={props.img} />
             </div>
             <div className='h-25 w-100 '>
-                <p className='' style={{color:"#03045E"}}><b>{props.name}</b> ({props.type})</p>
-                <p style={{color:"grey"}}>{props.details}</p>
+                <p className='' style={{ color: "#03045E" }}><b>{props.name}</b> ({props.type})</p>
+                <p style={{ color: "grey" }}>{props.details}</p>
             </div>
 
         </Link>
